3d189af1af8e6fe8d48513d54913356b6ada3ed3
ahsanali7893 <ahsan3916902@gmail.com>
2024-10-03 14:32:44 +0500

    initial changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule], 
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   text: string = '';
+  showToast: boolean = false;
 
   convertToUpperCase() {
     this.text = this.text.toUpperCase();
@@ -16,5 +19,38 @@ export class HomeComponent {
 
   convertToLowerCase() {
     this.text = this.text.toLowerCase();
+  }
+
+  removeExtraSpaces() {
+    this.text = this.text.trim().replace(/\s+/g, ' ');
+  }
+
+  copyText() {
+    if (this.text) {
+      navigator.clipboard.writeText(this.text).then(() => {
+        this.showToastMessage();
+      }).catch((err) => {
+        console.error('Could not copy text: ', err);
+      });
+    } else {
+      alert('No text to copy!');
+    }
+  }
+
+  speakText() {
+    if (this.text) {
+      const utterance = new SpeechSynthesisUtterance(this.text);
+      speechSynthesis.speak(utterance);
+    } else {
+      alert('No text to speak!');
+    }
+  }
+
+  // Show toast message
+  showToastMessage() {
+    this.showToast = true;
+    setTimeout(() => {
+      this.showToast = false;
+    }, 2000); // Hide the toast after 2 seconds
   }
 }
